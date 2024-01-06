fa82bd98313ea3e8b7b29078d738600ddfbf8312
Cintriago <cintriago@origamiec.com>
2024-01-06 11:48:57 -0500

    Init

---
+++ src/main/java/com/example/retoOne/RetoOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.retoOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/retoOne/RetoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retoOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
